14b29232c2b7af0ed8b91b008857f5cb05553273
timroerstroem <timroerstroem@gmail.com>
2016-12-13 08:18:54 +0100

    Cleaner runs.
    Included some checks to ensure that reformatted csv files are not parsed.

--- src/FormatCSV/format.py
+++ src/FormatCSV/format.py
@@ -6,15 +6,34 @@ Created on 21 Jul 2016
 import os
 import pandas
 
-work_dir = os.path.expanduser('~\\Downloads')
+work_dir = os.path.expanduser('~/Downloads')
 os.chdir(work_dir)
 
+
 def reformat(account):
-    accountData = pandas.read_csv(account, sep=';', usecols=[0, 2, 4], parse_dates=[0], dayfirst=True, quotechar='"', encoding='cp1252', header=0, names=['Date', 'Payee', 'Amount'])
-    accountData['Amount'] = accountData['Amount'].str.replace('.', '').str.replace(',', '.')
+    '''This function uses the pandas library to extract the required
+    information from a CSV file from presumably most Danish banks.
+    '''
+    accountData = pandas.read_csv(account, sep=';', usecols=['Dato', 'Tekst',
+                                                             'Beløb'],
+                                  parse_dates=[0], dayfirst=True,
+                                  quotechar='"', encoding='cp1252', header=0,
+                                  names=['Date', 'Payee', 'Amount'])
+    accountData['Amount'] = accountData['Amount'].str.replace('.', '')
+    accountData['Amount'] = accountData['Amount'].str.replace(',', '.')
     accountData.to_csv(account, index=False, encoding='utf-8')
-    
+
+
 for csvFilename in os.listdir('.'):
     if not csvFilename.endswith('.csv'):
-        continue # skip non-csv files
+        continue  # skip non-csv files
+
+    column_check = pandas.read_csv(csvFilename)
+
+    if len(column_check) == 3:
+        # If the file has only  three columns, we have already reformatted it.
+        print(csvFilename + " already reformatted, skipping.")
+        continue
+
+    print("Reformatting " + csvFilename + "...")
     reformat(csvFilename)
