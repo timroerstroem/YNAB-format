fe49d0297414417beef69501a3520e4275580658
timroerstroem <timroerstroem@gmail.com>
2016-12-13 08:21:09 +0100

    Code cleanup.

--- src/FormatCSV/format.py
+++ src/FormatCSV/format.py
@@ -14,26 +14,26 @@ def reformat(account):
     '''This function uses the pandas library to extract the required
     information from a CSV file from presumably most Danish banks.
     '''
-    accountData = pandas.read_csv(account, sep=';', usecols=['Dato', 'Tekst',
-                                                             'Beløb'],
-                                  parse_dates=[0], dayfirst=True,
-                                  quotechar='"', encoding='cp1252', header=0,
-                                  names=['Date', 'Payee', 'Amount'])
-    accountData['Amount'] = accountData['Amount'].str.replace('.', '')
-    accountData['Amount'] = accountData['Amount'].str.replace(',', '.')
-    accountData.to_csv(account, index=False, encoding='utf-8')
+    account_data = pandas.read_csv(account, sep=';', usecols=['Dato', 'Tekst',
+                                                              'Beløb'],
+                                   parse_dates=[0], dayfirst=True,
+                                   quotechar='"', encoding='cp1252', header=0,
+                                   names=['Date', 'Payee', 'Amount'])
+    account_data['Amount'] = account_data['Amount'].str.replace('.', '')
+    account_data['Amount'] = account_data['Amount'].str.replace(',', '.')
+    account_data.to_csv(account, index=False, encoding='utf-8')
 
 
-for csvFilename in os.listdir('.'):
-    if not csvFilename.endswith('.csv'):
+for csv_filename in os.listdir('.'):
+    if not csv_filename.endswith('.csv'):
         continue  # skip non-csv files
 
-    column_check = pandas.read_csv(csvFilename)
+    column_check = pandas.read_csv(csv_filename)
 
     if len(column_check) == 3:
         # If the file has only  three columns, we have already reformatted it.
-        print(csvFilename + " already reformatted, skipping.")
+        print(csv_filename + " already reformatted, skipping.")
         continue
 
-    print("Reformatting " + csvFilename + "...")
-    reformat(csvFilename)
+    print("Reformatting " + csv_filename + "...")
+    reformat(csv_filename)
